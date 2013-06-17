067dfbfd8d3ea4d6dab131e1ca3bef2170aee111
Jonathan Holloway <jonathan.holloway@gmail.com>
2013-06-18 00:27:02 +0200

    Update irc-commands.sh

--- irc-commands.sh
+++ irc-commands.sh
@@ -1,3 +1,10 @@
-Registering a new nickname:
+######Best Programming IRC Channels
+
+  #java
+  #scala
+
+
+#####Registering a new nickname:
   
   /msg NickServ register PASSWORD_GOES_HERE EMAIL_ADDRESS_GOES_HERE
+
