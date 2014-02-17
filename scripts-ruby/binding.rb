6b249712fd05ee519f789366f0b504a868816dc7
Jonathan Holloway <http://www.jonathanholloway.co.uk>
2014-02-17 06:53:35 +0000

    Removed

--- scripts-ruby/binding.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-def meth binding
-	eval "puts x", binding
-end
-
-x = 5
-meth binding
--- scripts-ruby/closures.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-def run_proc(p)
-	p.call
-end
-
-name ="Jon"
-printname = proc { puts name }
-run_proc printname
