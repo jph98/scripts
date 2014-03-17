6bd8c8298c6ea59e20b4ddae95f605bfaf64030d
Jonathan Holloway <jonathan.holloway@gmail.com>
2014-03-17 14:21:20 +0000

    Update javascript-ractive.js

--- javascript-ractive.js
+++ javascript-ractive.js
@@ -5,3 +5,13 @@ Makes use of:
 * Mustache for templating
 * Includes Ractive.js 
 * Script for doing the application of content to the template.
+
+Properties (very basic) - {{ name }}
+
+    var ractive = new Ractive({
+      el: 'container',
+      template: '#myTemplate',
+      data: { greeting: 'Hello', recipient: 'world' }
+    });
+
+Nested Properties - http://learn.ractivejs.org/nested-properties/1/
